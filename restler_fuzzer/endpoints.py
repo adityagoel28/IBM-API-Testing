a6c4f31c405c6a70346c3d44f97aedf7609a328c
Aditya Goel <adityagoel2002@gmail.com>
2023-03-04 00:15:14 +0530

    Create endpoints.py

---
+++ restler_fuzzer/endpoints.py
@@ -0,0 +1,27 @@
+# Petstore
+all_endpoints = [{'GET': '/v2/pet/findByStatus'}, {'GET': '/v2/pet/findByTags'}, {'POST': '/v2/store/order'}, {'GET': '/v2/store/inventory'}, {'GET': '/v2/store/order/'},
+                  {'DELETE': '/v2/store/order/'}, {'POST': '/v2/user/createWithArray'}, {'POST': '/v2/user/createWithList'}, {'GET': '/v2/user/login'}, {'GET': '/v2/user/logout'}, {'POST': '/v2/pet'},
+                  {'PUT': '/v2/pet'}, {'DELETE': '/v2/pet/'}, {'GET': '/v2/pet/'}, {'GET': '/v2/user/'}, {'PUT': '/v2/user/'}, {'DELETE': '/v2/user/'}, {'POST': '/v2/user'}, {'POST': '/v2/pet/'}]
+
+
+# FakerestAPI
+all_endpoints = [{'GET': '/api/v1/Activities'}, {'POST': '/api/v1/Activities'}, {'POST': '/api/v1/Users'}, {'PUT': '/api/v1/Activities/'}, {'DELETE': '/api/v1/Activities/'},
+                  {'GET': '/api/v1/Authors'}, {'POST': '/api/v1/Authors'}, {'GET': '/api/v1/Authors/authors/books/'}, {'GET': '/api/v1/Users'}, {'PUT': '/api/v1/Authors/'}, 
+                  {'DELETE': '/api/v1/Authors/'}, {'PUT': '/api/v1/Users'}, {'DELETE': '/api/v1/Users'},
+                  {'GET': '/api/v1/Books'}, {'POST': '/api/v1/Books'}, {'GET': '/api/v1/Books/'}, {'PUT': '/api/v1/Books/'}, {'DELETE': '/api/v1/Books/'},
+                  {'GET': '/api/v1/CoverPhotos'}, {'POST': '/api/v1/CoverPhotos'}, {'PUT': '/api/v1/CoverPhotos'}, {'DELETE': '/api/v1/CoverPhotos'}, 
+                  {'GET': '/api/v1/CoverPhotos/books/covers/'}]
+
+
+# BookCartAPI
+all_endpoints = [{'GET': '/api/Book/GetCategoriesList'}, {'DELETE': '/api/Book/GetSimilarBooks/'}, {'POST': '/api/CheckOut/'}, {'POST': '/api/Login'}, {'GET': '/api/Order/'}, 
+                 {'GET': '/api/ShoppingCart/SetShoppingCart/'}, {'POST': '/api/ShoppingCart/AddToCart'}, {'GET': '/api/ShoppingCart'}, {'DELETE': '/api/ShoppingCart'},  
+                 {'PUT': '/api/ShoppingCart'}, {'DELETE': '/api/ShoppingCart'}, {'GET': '/api/Book'}, {'POST': '/api/Book'}, {'PUT': '/api/Book'},  {'DELETE': '/api/Book'}, 
+                 {'GET': '/api/User/validateUserName'}, {'POST': '/api/User'}, {'GET': '/api/User'}, {'POST': '/api/Wishlist/ToggleWishlist'}, {'GET': '/api/Wishlist'}, {'DELETE': '/api/Wishlist'}]
+
+
+# xkcd
+all_endpoints = [{'GET': '/info.0.json'}, {'GET': '/{comicId}/info.0.json'}]
+
+
+# language_tool
